5caea8e104166f0728c38aa84255a1b416c6e7c1
Alex <1014104338@qq.com>
2020-07-26 18:26:02 +0800

    modification for testing net efficiency

--- test_two_stream.py
+++ test_two_stream.py
@@ -24,6 +24,7 @@ import tensorflow as tf
 from lib.config import config as cfg
 from lib.utils.nms_wrapper import nms
 from lib.utils.test import im_detect
+from lib.utils.test import test_net
 #from nets.resnet_v1 import resnetv1
 from lib.nets.vgg16 import vgg16
 from lib.utils.timer import Timer
@@ -36,70 +37,70 @@ NETS = {'vgg16': ('vgg16_faster_rcnn_iter_40000.ckpt',), 'res101': ('res101_fast
 DATASETS = {'pascal_voc': ('voc_2007_trainval',), 'pascal_voc_0712': ('voc_2007_trainval+voc_2012_trainval',)}
 count = 1
 
-def vis_detections(im, class_name, dets, thresh=0.5):
-    """Draw detected bounding boxes."""
-    inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        return
+# def vis_detections(im, class_name, dets, thresh=0.5):
+#     """Draw detected bounding boxes."""
+#     inds = np.where(dets[:, -1] >= thresh)[0]
+#     if len(inds) == 0:
+#         return
 
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
-    for i in inds:
-        bbox = dets[i, :4]
-        score = dets[i, -1]
+#     im = im[:, :, (2, 1, 0)]
+#     fig, ax = plt.subplots(figsize=(12, 12))
+#     ax.imshow(im, aspect='equal')
+#     for i in inds:
+#         bbox = dets[i, :4]
+#         score = dets[i, -1]
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-        )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
+#         ax.add_patch(
+#             plt.Rectangle((bbox[0], bbox[1]),
+#                           bbox[2] - bbox[0],
+#                           bbox[3] - bbox[1], fill=False,
+#                           edgecolor='red', linewidth=3.5)
+#         )
+#         ax.text(bbox[0], bbox[1] - 2,
+#                 '{:s} {:.3f}'.format(class_name, score),
+#                 bbox=dict(facecolor='blue', alpha=0.5),
+#                 fontsize=14, color='white')
 
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                 fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    # save detected image
-    global count
-    fig.savefig("./resulting_images/{}.png".format(count))
-    count += 1
-    plt.draw()
+#     ax.set_title(('{} detections with '
+#                   'p({} | box) >= {:.1f}').format(class_name, class_name,
+#                                                   thresh),
+#                  fontsize=14)
+#     plt.axis('off')
+#     plt.tight_layout()
+#     # save detected image
+#     global count
+#     fig.savefig("./resulting_images/{}.png".format(count))
+#     count += 1
+#     plt.draw()
 
 
-def demo(sess, net, image_name):
-    """Detect object classes in an image using pre-computed object proposals."""
+# def demo(sess, net, image_name):
+#     """Detect object classes in an image using pre-computed object proposals."""
 
-    # Load the demo image
-    im_file = os.path.join('lib/layer_utils', image_name)
-    im = cv2.imread(im_file)
+#     # Load the demo image
+#     im_file = os.path.join('lib/layer_utils', image_name)
+#     im = cv2.imread(im_file)
 
-    # Detect all object classes and regress object bounds
-    timer = Timer()
-    timer.tic()
-    scores, boxes = im_detect(sess, net, im)
-    timer.toc()
-    print('Detection took {:.3f}s for {:d} object proposals'.format(timer.total_time, boxes.shape[0]))
+#     # Detect all object classes and regress object bounds
+#     timer = Timer()
+#     timer.tic()
+#     scores, boxes = im_detect(sess, net, im)
+#     timer.toc()
+#     print('Detection took {:.3f}s for {:d} object proposals'.format(timer.total_time, boxes.shape[0]))
 
-    # Visualize detections for each class
-    CONF_THRESH = 0.1
-    NMS_THRESH = 0.1
+#     # Visualize detections for each class
+#     CONF_THRESH = 0.1
+#     NMS_THRESH = 0.1
 
-    for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1  # because we skipped background
-        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
-        cls_scores = scores[:, cls_ind]
-        dets = np.hstack((cls_boxes,
-                          cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
-        dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+#     for cls_ind, cls in enumerate(CLASSES[1:]):
+#         cls_ind += 1  # because we skipped background
+#         cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
+#         cls_scores = scores[:, cls_ind]
+#         dets = np.hstack((cls_boxes,
+#                           cls_scores[:, np.newaxis])).astype(np.float32)
+#         keep = nms(dets, NMS_THRESH)
+#         dets = dets[keep, :]
+#         vis_detections(im, cls, dets, thresh=CONF_THRESH)
 
 
 def parse_args():
@@ -145,10 +146,14 @@ if __name__ == '__main__':
 
     print('Loaded network {:s}'.format(tfmodel))
 
-    for file in os.listdir("./lib/layer_utils"):
-        if file.endswith(".jpg"):
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print('Demo for lib/layer_utils/{}'.format(file))
-            demo(sess, net, file)
+    # for file in os.listdir("./lib/layer_utils"):
+    #     if file.endswith(".jpg"):
+    #         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    #         print('Demo for lib/layer_utils/{}'.format(file))
+    #         demo(sess, net, file)
 
-    plt.show()
+    # plt.show()
+
+    test_net(sess, net, imdb, filename, max_per_image=args.max_per_image)
+
+sess.close()
